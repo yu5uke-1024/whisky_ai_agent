c8529de18f907d6fa2e9b865ec04014c0f13b894
Yusuke <yskamto1024@gmail.com>
2025-05-19 23:42:31 +0900

    add structed schema

--- source/agent.py
+++ source/agent.py
@@ -1,5 +1,15 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from pydantic import BaseModel, Field
+
+class WhiskyInfo(BaseModel):
+    name: str = Field(
+        description="ウイスキーの名称。ブランド名と商品名を含めてください（例：'アードベッグ 10年'）。"
+    )
+    description: str = Field(
+        description="ウイスキーの詳細な説明。香り、味わい、余韻、産地、蒸溜所情報、特徴などを含めてください。"
+    )
+
 
 root_agent = Agent(
     name="whisky_agent",
@@ -7,10 +17,30 @@ root_agent = Agent(
     description="whisky ai agent",
     instruction="""
     あなたはウイスキーに詳しいバーテンダーです。\n
-    ユーザーの質問に簡潔に答えて。必要に応じてgoogle_searchツールを使ってください。
+    ユーザーの質問におすすめのウイスキーをレクチャーしてください。
+    重要: あなたの回答は、以下の構造に一致する有効なJSON形式である必要があります:
+    {
+        "name": "ここにウイスキーの名称を入力してください",
+        "description": "ここにウイスキーの詳細説明を記載してください。香り、味わい、余韻、産地、蒸溜所情報、特徴などを含めてください。"
+    }
+    説明文や補足など、JSON以外のテキストは絶対に含めないでください。
     """,
-    tools=[google_search],
+    output_schema=WhiskyInfo,
+    output_key="whisky",
 )
+
+
+# root_agent = Agent(
+#     name="whisky_agent",
+#     model="gemini-2.0-flash-lite", #"gemini-1.5-flash",
+#     description="whisky ai agent",
+#     instruction="""
+#     あなたはウイスキーに詳しいバーテンダーです。\n
+#     ユーザーの質問に簡潔に答えて。必要に応じてgoogle_searchツールを使ってください。
+#     また、おすすめの
+#     """,
+#     tools=[google_search],
+# )
 
 
 # def get_current_time() -> dict:
